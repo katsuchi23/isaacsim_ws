6dfdcd26cee9c540b36b492111c144eccb83b057
katsuchi23 <reynaldywidjaja3@gmail.com>
2025-05-30 08:05:09 +0800

    init commit isaacsim working space

---
+++ isaac_robot_description/launch/robot_display_launch.py
@@ -0,0 +1,69 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('isaac_robot_description')
+    default_model_path = os.path.join(pkg_share, 'urdf', 'robot.urdf')
+    default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'config.rviz')
+
+    # Declare the launch arguments
+    model_arg = DeclareLaunchArgument(
+        'model',
+        default_value=default_model_path,
+        description='Absolute path to robot model file'
+    )
+    rvizconfig_arg = DeclareLaunchArgument(
+        'rvizconfig',
+        default_value=default_rviz_config_path,
+        description='Absolute path to rviz config file'
+    )
+    gui_arg = DeclareLaunchArgument(
+        'gui',
+        default_value='True',
+        description='Flag to enable joint_state_publisher_gui'
+    )
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+    )
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', default_model_path])}],
+        condition=UnlessCondition(LaunchConfiguration('gui'))
+    )
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        condition=IfCondition(LaunchConfiguration('gui'))
+    )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+    ld = LaunchDescription()
+
+    ld.add_action(model_arg)
+    ld.add_action(rvizconfig_arg)
+    ld.add_action(gui_arg)
+    
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(joint_state_publisher_node)
+    ld.add_action(joint_state_publisher_gui_node)
+    ld.add_action(rviz_node)
+
+    return ld
