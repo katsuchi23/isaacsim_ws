5a1b60110b1f45440c6a8091370e3f4aef7148b5
katsuchi23 <reynaldywidjaja3@gmail.com>
2025-06-04 19:46:30 +0800

    changing prim to leatherback

--- isaac_robot_controller/scripts/cmdvel_to_ackermann.py
+++ isaac_robot_controller/scripts/cmdvel_to_ackermann.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
 #
@@ -24,7 +24,7 @@ class CmdvelToAckermann(Node):
         super().__init__('cmdvel_to_ackermann')
 
         self.declare_parameter('publish_period_ms', 20)
-        self.declare_parameter('track_width', 0.31)
+        self.declare_parameter('track_width', 0.24)
         self.declare_parameter('acceleration', 0.0)
         self.declare_parameter('steering_velocity', 0.0)
         
